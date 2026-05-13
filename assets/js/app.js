b7f0580362c97e1cfbafd51793f70599ea60c988
Nathan <nathansbarros2005@gmail.com>
2026-05-13 12:10:58 -0300

    fix: renomeando login.html para index.html

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const paginaAtual = window.location.pathname;
 
   if (
-    paginaAtual.includes("index.html") ||
+    paginaAtual.includes("home.html") ||
     paginaAtual === "/" ||
     paginaAtual === ""
   )
@@ -31,7 +31,7 @@ function atualizarMenu() {
   if (token) {
     // Usuário LOGADO
     navButtons.innerHTML = `
-            <button onclick="window.location.href='index.html'">Área do Estudante</button>
+            <button onclick="window.location.href='home.html'">Área do Estudante</button>
             <button onclick="window.location.href='meus-emprestimos.html'">Meu Histórico</button>
             <button onclick="window.location.href='painel-biblio.html'">Área do Funcionário</button>
             <button onclick="fazerLogout()" style="background-color: #e74c3c; margin-left: 20px;">Sair</button>
@@ -39,7 +39,7 @@ function atualizarMenu() {
   } else {
     // Usuário DESLOGADO
     navButtons.innerHTML = `
-            <button onclick="window.location.href='login.html'" style="background-color: #1abc9c;">Fazer Login</button>
+            <button onclick="window.location.href='index.html'" style="background-color: #1abc9c;">Fazer Login</button>
         `;
   }
 }
@@ -63,7 +63,7 @@ function protegerPaginas() {
   const token = localStorage.getItem("token_biblioteca");
   const paginaAtual = window.location.pathname;
   const paginasProtegidas = [
-    "index.html",
+    "home.html",
     "cadastrar-livro.html",
     "meus-emprestimos.html",
     "painel-biblio.html",
@@ -75,7 +75,7 @@ function protegerPaginas() {
 
   if (precisaProteger && !token) {
     alert("Você precisa fazer login para acessar o sistema.");
-    window.location.href = "login.html";
+    window.location.href = "index.html";
   }
 }
 
@@ -95,7 +95,7 @@ async function logarUsuario() {
 
     if (dados.token) {
       localStorage.setItem("token_biblioteca", dados.token);
-      window.location.href = "index.html"; // Vai para a tela inicial
+      window.location.href = "home.html"; // Vai para a tela inicial
     } else {
       alert(
         "Erro: " + (dados.erro || "Falha no login. Verifique suas credenciais.")
@@ -109,7 +109,7 @@ async function logarUsuario() {
 
 function fazerLogout() {
   localStorage.removeItem("token_biblioteca");
-  window.location.href = "login.html";
+  window.location.href = "index.html";
 }
 
 async function cadastrarUsuario() {
@@ -132,7 +132,7 @@ async function cadastrarUsuario() {
 
     if (resposta.ok) {
       alert("Conta criada com sucesso! Agora você pode fazer o login.");
-      window.location.href = "login.html"; // Manda o usuário para a tela de login
+      window.location.href = "index.html"; // Manda o usuário para a tela de login
     } else {
       alert("Erro ao criar conta: " + (dados.erro || dados.mensagem));
     }
@@ -346,7 +346,7 @@ async function salvarLivro() {
 
     if (resposta.ok) {
       alert("Livro cadastrado com sucesso!");
-      window.location.href = "index.html";
+      window.location.href = "home.html";
     } else {
       alert("Erro: " + (resultado.erro || "Falha ao cadastrar"));
     }
